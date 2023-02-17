0be1215a3c542b333c46d3311b10a49e0655e7ab
WizMe-M <timkin.moxim@mail.ru>
2023-02-17 13:10:58 +0300

    API global usings

--- AutoDealer.API/GlobalUsings.cs
+++ AutoDealer.API/GlobalUsings.cs
@@ -1,4 +1,6 @@
 ﻿// Global using directives
 
 global using AutoDealer.DAL.Database;
+global using AutoDealer.DAL.Database.Entity;
+global using AutoDealer.DAL.Database.Enum;
 global using Microsoft.EntityFrameworkCore;
