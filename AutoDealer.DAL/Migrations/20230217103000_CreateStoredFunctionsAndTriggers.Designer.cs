482f141138248e69dd3fbc19c81d43cfcd2da0fe
WizMe-M <timkin.moxim@mail.ru>
2023-02-17 10:45:47 +0300

    fixed migration with stored procedures and triggers

---
+++ AutoDealer.DAL/Migrations/20230217103000_CreateStoredFunctionsAndTriggers.Designer.cs
@@ -0,0 +1,10 @@
+﻿using AutoDealer.DAL.Database;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AutoDealer.DAL.Migrations;
+
+[DbContext(typeof(AutoDealerContext))]
+[Migration("20230217103000_CreateStoredFunctionsAndTriggers")]
+public partial class CreateStoredFunctionsAndTriggers {
+}
