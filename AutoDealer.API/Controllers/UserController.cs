49f7d8e47b2e312d83df86a56a7c39dd97aeb1cd
WizMe-M <timkin.moxim@mail.ru>
2023-02-19 19:59:18 +0300

    use response types and action results

--- AutoDealer.API/Controllers/UserController.cs
+++ AutoDealer.API/Controllers/UserController.cs
@@ -12,50 +12,91 @@ public class UserController : ControllerBase
     }
 
     [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<User>), StatusCodes.Status200OK)]
     public IEnumerable<User> GetAll()
     {
         return _repository.Get();
     }
 
     [HttpGet("{id:int}")]
-    public User? GetById(int id)
+    [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult GetById(int id)
     {
-        return _repository.Get(id);
+        var user = _repository.Get(id);
+        return user is { } ? Ok(user) : NotFound();
     }
 
     [HttpPost("{id:int}/create")]
-    public void CreateUser([FromRoute] int id, [FromBody] NewUser newUser)
+    [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [Consumes("application/json")]
+    public IActionResult CreateUser([FromRoute] int id, [FromBody] NewUser newUser)
     {
+        if (newUser is not { Login: { }, Password: { } }) return NoContent();
+
         var user = new User
         {
             IdEmployee = id,
             Login = newUser.Login,
             PasswordHash = newUser.Password
         };
-        _repository.Create(user);
+
+        try
+        {
+            var created = _repository.Create(user);
+            return Ok(created);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
     }
 
     [HttpPatch("{id:int}/change-password")]
-    public void ChangePassword(int id, [FromBody] string passwordHash)
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [Consumes("application/json")]
+    public IActionResult ChangePassword(int id, [FromBody] string passwordHash)
     {
         var user = _repository.Get(id);
-        if (user is null) return;
+        if (user is null) return NotFound();
         user.PasswordHash = passwordHash;
         _repository.Update(user);
+        return Ok("User's password was changed");
     }
 
     [HttpPatch("{id:int}/restore")]
-    public void RestoreUser(int id)
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult RestoreUser(int id)
     {
         var user = _repository.Get(id);
-        if (user is null) return;
+        if (user is null) return NotFound();
+
+        if (!user.Deleted) return BadRequest("User IS NOT deleted");
         user.Deleted = false;
+
         _repository.Update(user);
+        return Ok("User was updated");
     }
 
     [HttpDelete("{id:int}/delete")]
-    public void DeleteUser(int id)
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult DeleteUser(int id)
     {
-        _repository.Delete( id);
+        var user = _repository.Get(id);
+        if (user is null) return NotFound();
+
+        if (user.Deleted) return BadRequest("User IS ALREADY deleted");
+
+        _repository.Delete(user);
+        return Ok("User was deleted");
     }
 }
