bfb17bc8ab15ba5213122bb561ae9fa7c8f10bbc
WizMe-M <timkin.moxim@mail.ru>
2023-02-26 15:44:28 +0300

    margin controller

---
+++ AutoDealer.API/BodyTypes/MarginIdentifier.cs
@@ -0,0 +1,3 @@
+﻿namespace AutoDealer.API.BodyTypes;
+
+public record MarginIdentifier(int CarModelId, DateOnly StartsFrom);
---
+++ AutoDealer.API/Controllers/MarginController.cs
@@ -0,0 +1,64 @@
+﻿namespace AutoDealer.API.Controllers;
+
+[ApiController]
+[Route("margins")]
+public class MarginController : DbContextController
+{
+    public MarginController(AutoDealerContext context) : base(context)
+    {
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var margins = Context.Margins
+            .Include(margin => margin.CarModel)
+            .ToArray();
+        return Ok(margins);
+    }
+
+    [HttpGet("{carModelId:int}")]
+    public IActionResult GetMarginsForModel(int carModelId)
+    {
+        var margins = Context.Margins
+            .Where(margin => margin.IdCarModel == carModelId)
+            .Include(margin => margin.CarModel)
+            .ToArray();
+        return Ok(margins);
+    }
+
+    [HttpGet("get-in-range")]
+    public IActionResult GetMarginsInRange(DateOnly from, DateOnly? to)
+    {
+        var endDate = to ?? DateOnly.MaxValue;
+
+        var margins = Context.Margins
+            .Where(margin => from <= margin.StartDate && margin.StartDate <= endDate)
+            .Include(margin => margin.CarModel)
+            .ToArray();
+        return Ok(margins);
+    }
+
+    [HttpPost("set-margin")]
+    public async Task<IActionResult> SetMargin([FromBody] MarginData marginData)
+    {
+        await Context.ExecuteSetMargin(marginData.CarModelId, marginData.StartsFrom, marginData.MarginValue);
+
+        return Ok("Margin was successfully set");
+    }
+
+    [HttpDelete("delete")]
+    public IActionResult Delete([FromBody] MarginIdentifier identifier)
+    {
+        var found = Context.Margins.FirstOrDefault(margin =>
+            margin.IdCarModel == identifier.CarModelId &&
+            margin.StartDate == identifier.StartsFrom);
+
+        if (found is null) return NotFound();
+
+        Context.Margins.Remove(found);
+        Context.SaveChanges();
+
+        return Ok("Margin was successfully deleted");
+    }
+}
