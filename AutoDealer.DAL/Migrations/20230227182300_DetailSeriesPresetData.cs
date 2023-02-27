74055026e59404b5fd354e71b952849b12cae29d
WizMe-M <timkin.moxim@mail.ru>
2023-02-27 21:24:12 +0300

    db migration 12

---
+++ AutoDealer.DAL/Migrations/20230227182300_DetailSeriesPresetData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoDealer.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class DetailSeriesPresetData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "detail_series",
+                columns: new[] { "id_detail_series", "code", "description" },
+                values: new object[] { 1, "SDH-242-790.1", "Full completed and assembled automobile" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "detail_series",
+                keyColumn: "id_detail_series",
+                keyValue: 1);
+        }
+    }
+}
