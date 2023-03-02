4e09a3cbf9c28f1788696c6ae79295e2f8c82322
WizMe-M <timkin.moxim@mail.ru>
2023-03-02 11:48:42 +0300

    db migration 15

---
+++ AutoDealer.DAL/Migrations/20230302084800_FixAutoStatusDefaultValue.cs
@@ -0,0 +1,39 @@
+﻿using AutoDealer.DAL.Database.Entity;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoDealer.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixAutoStatusDefaultValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<AutoStatus>(
+                name: "status",
+                table: "autos",
+                type: "auto_status",
+                nullable: false,
+                defaultValueSql: "'assembled'",
+                oldClrType: typeof(AutoStatus),
+                oldType: "auto_status",
+                oldDefaultValueSql: "'in_assembly'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<AutoStatus>(
+                name: "status",
+                table: "autos",
+                type: "auto_status",
+                nullable: false,
+                defaultValueSql: "'in_assembly'",
+                oldClrType: typeof(AutoStatus),
+                oldType: "auto_status",
+                oldDefaultValueSql: "'assembled'");
+        }
+    }
+}
