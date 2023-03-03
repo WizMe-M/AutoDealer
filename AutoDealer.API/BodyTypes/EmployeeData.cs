06cb1269eb70438fdaa8ff07d19376fb80e5f7d7
WizMe-M <timkin.moxim@mail.ru>
2023-03-03 15:18:15 +0300

    human data

--- AutoDealer.API/BodyTypes/EmployeeData.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AutoDealer.API.BodyTypes;
-
-public record EmployeeData(FullName FullName, Passport Passport, Post Post);
-
-public record FullName(string FirstName, string LastName, string? MiddleName);
-
-public record Passport(string Series, string Number);
