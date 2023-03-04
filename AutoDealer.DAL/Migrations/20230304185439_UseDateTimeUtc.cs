3628b82220fb1addf4bcf4f945ff82f58c9c2daf
WizMe-M <timkin.moxim@mail.ru>
2023-03-04 21:55:41 +0300

    db migration 28

---
+++ AutoDealer.DAL/Migrations/20230304185439_UseDateTimeUtc.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoDealer.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UseDateTimeUtc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "execution_date",
+                table: "sales",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "now() at time zone 'utc'",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "log_time",
+                table: "logs",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "now() at time zone 'utc'",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "conclusion_date",
+                table: "contracts",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "now() at time zone 'utc'",
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "assembly_date",
+                table: "autos",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "now() at time zone 'utc'",
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldDefaultValueSql: "now()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "execution_date",
+                table: "sales",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "now() at time zone 'utc'");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "log_time",
+                table: "logs",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "now() at time zone 'utc'");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "conclusion_date",
+                table: "contracts",
+                type: "date",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "now() at time zone 'utc'");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "assembly_date",
+                table: "autos",
+                type: "date",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "now() at time zone 'utc'");
+        }
+    }
+}
