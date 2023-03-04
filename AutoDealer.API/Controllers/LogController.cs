c0a712d5ca3a7ded625eab35d086417bf39e026c
WizMe-M <timkin.moxim@mail.ru>
2023-03-04 18:00:29 +0300

    logs

---
+++ AutoDealer.API/Controllers/LogController.cs
@@ -0,0 +1,20 @@
+﻿namespace AutoDealer.API.Controllers;
+
+[Authorize(Roles = nameof(Post.DatabaseAdmin))]
+[ApiController]
+[Route("logs")]
+public class LogController : DbContextController<Log>
+{
+    public LogController(AutoDealerContext context) : base(context)
+    {
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var logs = Context.Logs.ToArray();
+        return Ok("Logs listed", logs);
+    }
+
+    protected override Task LoadReferencesAsync(Log entity) => throw new NotSupportedException();
+}
